394710e5d806e58cd7daf092dcaf272b205e76fd
J <josephmonge00@gmail.com>
2023-11-30 20:58:43 -0800

    Implement account deletion

--- src/app/settings/delete-account/page.jsx
+++ src/app/settings/delete-account/page.jsx
@@ -1,10 +1,22 @@
 'use client';
 
+import { useContext } from 'react';
+import { UserContext } from '../../../user-provider';
+import { useRouter } from 'next/navigation';
+
 export default function DeleteAccountPage() {
+    const { user, setUser } = useContext(UserContext);
+    const router = useRouter()
     const handleClick = async () => {
         await fetch('/auth/delete-account', {
-            method: 'POST'
+            method: 'POST',
+            body: JSON.stringify({ userId: user.id }),
         });
+        await fetch(`/auth/logout`, {
+            method: 'POST',
+        });
+        setUser(null);
+        router.push('/');
     }
 
     return (
