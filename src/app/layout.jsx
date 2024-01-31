2d71a1f3b5f40e77c96ac0ec74a46c50bf59dde8
J <josephmonge00@gmail.com>
2024-01-31 15:21:05 -0800

    Animate background

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,6 +20,9 @@ export default async function RootLayout({ children }) {
     return (
         <html lang="en" className={publicSans.className}>
             <body>
+                <div className={styles["bgl"]}></div>
+                <div className={styles["bgm"]}></div>
+                <div className={styles["bgr"]}></div>
                 <div className={styles["bgtop"]}></div>
                 <UserProvider>
                     <Navbar />
