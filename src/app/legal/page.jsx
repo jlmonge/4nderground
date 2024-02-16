11daeeed5df8418a79809117a13776bea25efacc
J <josephmonge00@gmail.com>
2024-02-15 17:07:54 -0800

    Update legal page

--- src/app/legal/page.jsx
+++ src/app/legal/page.jsx
@@ -4,7 +4,7 @@ export default function LegalPage() {
     return (
         <div className={styles["textpage"]}>
             <h2 className={styles["h2"]}>Legal</h2>
-            <p>We do not condone the posting of copyrighted material on the site. You must own what you post. <a href="mailto:info@4nderground.com">Contact us</a> for further inquiries.</p>
+            <p>We do not condone the posting of copyrighted material on the site. You must own everything you upload. <a href="mailto:info@4nderground.com">Contact us</a> for further inquiries.</p>
         </div>
     );
 }
